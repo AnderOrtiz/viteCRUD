92f1bca016f45bf45a5ef6872411c77961055794
Anderson Ortiz <ortizone620@gmail.com>
2025-05-11 18:29:12 -0600

    Caso de uso - Crear usuario

---
+++ src/users/use-cases/save-user.js
@@ -0,0 +1,38 @@
+import { User } from "../models/user"
+
+/**
+ * 
+ * @param {Like<User>} userLike 
+ */
+const saveUser = async(userLike) => {
+
+    const user = new User( userLike );
+
+    //TODO: aquí falta un mapper
+
+    if (user.id) {
+        throw 'No implementada la actualización'
+    }
+
+    const updateUser = await createUser (user);
+    return updateUser;
+}
+
+/**
+ * @param {Like<User>} user
+ */
+const createUser = async(user) => {
+    const url = `${import.meta.env.VITE_BASE_URL}/users`;
+    const res = await fetch(url, {
+        method: 'POST',
+        body: JSON.stringify(user),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+
+    const newUser = await res.json();
+    console.log({newUser})
+
+    return newUser;
+}
